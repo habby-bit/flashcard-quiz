eaf91391a221b40b5ffca181041c946b5054cfe8
Habby Olusesi <habbyolu@gmail.com>
2021-07-14 16:08:20 -0400

    flex style, categories

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import FlashcardList from "./FlashcardList";
 import './app.css';
+import axios from 'axios';
 
 function App() {
 
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+  const [categories, setCategories] = useState([])
+  const categoryEl = useRef();
+  const amountEl = useRef();
+   
+  useEffect(() => {
+    axios.get('https://opentdb.com/api_category.php')
+    .then(res => {
+      setCategories(res.data.trivia_categories)
+    }, [])
+  })
+  useEffect(() => {
+    axios.get('https://opentdb.com/api.php?amount=10')
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = questionItem.correct_answer;
+        const options = [...questionItem.incorrect_answers.map(a => decodeString(a)), answer];
+
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: decodeString(answer),
+          options: options.sort(() => Math.random() - .5)
+        }
+      }))
+    })
+  }, [])
+
+  const decodeString = (str) => {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
 
   return(
-    <FlashcardList flashcards={flashcards} />
+    <>
+    <form classNmae="header" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="category">Category</label>
+        <select id="category" ref={categoryEl}>
+          {categories.map(category => {
+          return <option value={category.id} key={category.id}>{category.name}</option>
+          })}
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="amount">Number of Questions</label>
+        <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl}/>
+      </div>
+      <div class="form-group">
+        <button className="btn">Generate</button>
+      </div>
+    </form>
+    
+      <div className="container">
+        <FlashcardList flashcards={flashcards} />
+      </div>
+    </>
   );
 }
 
